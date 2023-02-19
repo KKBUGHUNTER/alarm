b6d8f1f348bda0b5900e14934e4e3449de1a61b3
karthikeyan <karthikeyanit7020@gmail.com>
2023-02-19 23:50:21 +0530

    karthikeyan

--- lib/home.dart
+++ lib/home.dart
@@ -5,7 +5,6 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
       body: const Center(
         child: Text('Boopathy Raj Develope This page'),
         
