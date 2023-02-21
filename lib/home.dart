f568f7e45a3cb2a1bfbd33c7ea94663c23550f9e
karthikeyan <karthikeyanit7020@gmail.com>
2023-02-21 17:32:55 +0530

    image add

--- lib/home.dart
+++ lib/home.dart
@@ -2,11 +2,14 @@ import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
+
   @override
   Widget build(BuildContext context) {
+    // ignore: prefer_const_constructors
     return Scaffold(
-      body: const Center(
-        child: Text('Boopathy Raj Develope This page'),
+      // ignore: prefer_const_constructors
+      body: Container(
+        child: Image(image: AssetImage("images/backg.jpg")),
       ),
     );
   }
